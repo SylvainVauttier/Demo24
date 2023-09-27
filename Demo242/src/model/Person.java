f74d2399d8404bfaf00aa9217308629e60f744c4
Sylvain Vauttier <sylvain.vauttier@mines-ales.fr>
2023-09-27 16:39:40 +0200

    Get set age

--- Demo242/src/model/Person.java
+++ Demo242/src/model/Person.java
@@ -16,6 +16,14 @@ public class Person {
 	int age;
 
 
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	int weight;
 
 	public int getWeight() {
